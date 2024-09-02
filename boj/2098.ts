51e6f6710a6537eb35098661f4edb3033b0d3b55
kw0n0 <alwayskw0n0@gmail.com>
2024-09-02 09:11:29 +0900

    refactor: lodash를 이용한 리팩토링

--- boj/2098.ts
+++ boj/2098.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 const fs = require('node:fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
@@ -6,9 +7,7 @@ const graph = input.map((v: string) => {
   return v.split(' ').map(Number);
 });
 
-const dp = Array.from({ length: N }, () =>
-  Array.from({ length: 1 << N }, () => 0)
-);
+const dp = _.times(N, () => _.times(1 << N, () => 0));
 
 function dfs(x: number, visited: number) {
   //종료조건: 모두 방문했고 경로가 있다면 현재지점에서 출발점으로의 비용 반환
@@ -25,9 +24,9 @@ function dfs(x: number, visited: number) {
   dp[x][visited] = Number.POSITIVE_INFINITY;
 
   //메인로직: 현재 지점에서 가능한 모든 경로순회
-  for (let i = 1; i < N; i++) {
-    //경로가 없거나 이미 방문한 지점이라면
-    if (graph[x][i] === 0 || visited & (1 << i)) continue;
+  _.forEach(_.range(1, N), (i) => {
+    // 경로가 없거나 이미 방문한 지점이라면
+    if (graph[x][i] === 0 || visited & (1 << i)) return;
 
     //메모이제이션된 값과 비교
     dp[x][visited] = Math.min(
@@ -35,7 +34,8 @@ function dfs(x: number, visited: number) {
       //현재지점과 방문지점을 갱신 + 이후 만들어질 하위트리의 최적값에 현재지점까지의 비용 더하기
       dfs(i, visited | (1 << i)) + graph[x][i]
     );
-  }
+  });
+
   return dp[x][visited];
 }
 
